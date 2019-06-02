ab1aff78f373a9494d563d977b38ef4375d681a1
Pere Urbon <pere@confluent.io>
2019-06-02 08:47:15 +0200

    add rabbitmq connector

--- scripts/create-rabbitmq-source.sh
+++ scripts/create-rabbitmq-source.sh
@@ -3,28 +3,12 @@
 curl -i -X POST -H "Accept:application/json" \
     -H  "Content-Type:application/json" http://kafka-connect-cp:18083/connectors/ \
     -d '{
-      "name": "mysql-source-demo-customers",
+      "name": "rabbitmq-source-connector",
       "config": {
-            "connector.class": "io.debezium.connector.mysql.MySqlConnector",
-            "database.hostname": "mysql",
-            "database.port": "3306",
-            "database.user": "debezium",
-            "database.password": "dbz",
-            "database.server.id": "42",
-            "database.server.name": "asgard",
-            "table.whitelist": "demo.customers",
-            "database.history.kafka.bootstrap.servers": "kafka:29092",
-            "database.history.kafka.topic": "dbhistory.demo" ,
-            "include.schema.changes": "true",
-            "key.converter":"org.apache.kafka.connect.json.JsonConverter",
-            "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-            "value.converter.schemas.enable": false,
-            "transforms": "unwrap,InsertTopic,InsertSourceDetails",
-            "transforms.unwrap.type": "io.debezium.transforms.UnwrapFromEnvelope",
-            "transforms.InsertTopic.type":"org.apache.kafka.connect.transforms.InsertField$Value",
-            "transforms.InsertTopic.topic.field":"messagetopic",
-            "transforms.InsertSourceDetails.type":"org.apache.kafka.connect.transforms.InsertField$Value",
-            "transforms.InsertSourceDetails.static.field":"messagesource",
-            "transforms.InsertSourceDetails.static.value":"Debezium CDC from MySQL on asgard"
+         "name" : "rabbitmq-source-connector",
+         "connector.class" : "io.confluent.connect.rabbitmq.RabbitMQSourceConnector",
+         "tasks.max" : "1",
+         "kafka.topic" : "foo.bar",
+         "rabbitmq.queue" : "queue.foo.bar"
        }
     }'
